edc9e308ada1339773ac913b30b73872c191896c
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-04-20 17:37:03 +0600

    feat: update metadata title, implement AddTask component, and enhance ThemToggle with Switch

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,4 +1,6 @@
+import { AddTask } from '@/components/AddTask';
 import TasksCard from '@/components/TasksCard';
+import { createATask } from '@/lib/actions';
 import { getTask } from '@/lib/task';
 import React from 'react';
 
@@ -9,6 +11,7 @@ const TasksPage = async() => {
     return (
         <div>
             <h2>Task: {tasks.length} </h2>
+            <AddTask createATask={createATask} />
             <div className='grid md:grid-cols-3 gap-4 p-4'>
                 {
                     tasks.map(task=> <TasksCard key={task.id} task={task}/>)
